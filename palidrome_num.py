d0adc3d300172d6a3d3af81f47b6d0b51f92ccfe
shubhamkathe-datascientist <shubhamkathe23@gmail.com>
2023-02-15 17:07:47 +0530

    Push Files

---
+++ palidrome_num.py
@@ -0,0 +1,17 @@
+n=int(input("enter num"))
+i=n
+count=0
+while i<0:
+    count=count+1
+    i//=10
+i=n
+sum=0
+digit=0
+while i>0:
+    digit=digit+i%10
+    pro=1
+    x=1
+    while x<=count:
+        pro=pro+digit
+        
+        
